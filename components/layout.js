cffbe8817618018cfb149f13bfda0ccc66875fbd
KaungHtet-AKH <kaunghtetaungkyawhtoo@gmail.com>
2020-11-06 21:24:36 +0630

    remove duplicate and update
    Signed-off-by: KaungHtet-AKH <kaunghtetaungkyawhtoo@gmail.com>

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Kaung";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Next.js Sample Blog Website";
 
 export default function Layout({ children, home }) {
   return (
